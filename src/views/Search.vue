1624bd5ca0eb5013b7222f6406efded63670b053
KulaWeb <artur.kulinski87@gmail.com>
2019-06-19 14:41:14 +0200

    Enough

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,14 +2,40 @@
    <div class="wrapper">
        <div class="search">
            <label for="search">Search</label>
-           <input name="search" id="search" type="search">
+           <input name="search" id="search" type="search" v-model="searchValue" @input="handleInput">
        </div>
+       <ul>
+           <li v-for="item in results" :key="item.data[0].nasa_id">
+                {{ item.data[0].description}}
+           </li>
+       </ul>
    </div>
 </template>
 
 <script>
+    import axios from 'axios';
+    import debounce from 'lodash.debounce';
+    const API = 'https://images-api.nasa.gov/search';
+
 export default {
   name: 'Search',
+  data() {
+    return {
+      searchValue: '',
+      results: [],
+    };
+  },
+  methods: {
+    handleInput: debounce(function () {
+      axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items;
+        }).catch((error) => {
+        console.log(error);
+      });
+    },500),
+
+  },
 };
 </script>
 
